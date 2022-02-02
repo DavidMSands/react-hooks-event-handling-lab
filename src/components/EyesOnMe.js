ad92bd697f05efbb8e1cbfc017522adda33266b2
David <Davidmaxsands@gmail.com>
2022-02-01 23:00:46 -0700

    commit tree 99e44c6c5cf58e087cb2df28f23483656ec20c5d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function focusHandler() {
+    console.log('Good!')
+}
+
+const blurHandler = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+  return (
+      <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe;
